f0368ff07b2e01e4218ec40fa2bfb1e31b2c3d94
Brian-mula <mulatiwangilabrian@gmail.com>
2021-07-17 14:08:31 +0300

    second commit

--- src/components/Dummydata.js
+++ src/components/Dummydata.js
@@ -4,32 +4,28 @@ const projects = [
     name: "Television",
     cost: 3000,
     duration: "30 days",
-    photo:
-      "https://cdn.pixabay.com/photo/2017/02/12/13/04/lcd-2059995__340.png",
+    photo: "",
   },
   {
     id: 2,
     name: "Life",
     cost: 30000,
     duration: "30 days",
-    photo:
-      "https://cdn.pixabay.com/photo/2015/07/02/09/52/roundtable-828546__340.jpg",
+    photo: "",
   },
   {
     id: 3,
     name: "Fee",
     cost: 20000,
     duration: "30 days",
-    photo:
-      "https://cdn.pixabay.com/photo/2017/02/20/14/50/watching-2082788__340.png",
+    photo: "",
   },
   {
     id: 4,
     name: "Writer dom",
     cost: 300000,
     duration: "30 days",
-    photo:
-      "https://cdn.pixabay.com/photo/2017/08/05/12/08/living-room-2583032__340.jpg",
+    photo: "",
   },
 ];
 export default projects;
